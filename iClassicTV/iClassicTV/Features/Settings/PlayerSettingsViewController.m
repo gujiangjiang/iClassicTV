ad3f206881d8dac29fa849ecca3d21bf5dc6e815
orthomdcn <gujiangjiang@qq.com>
2026-04-25 15:03:36 +0800

    [优化] 播放器选择与默认全屏逻辑改回下拉菜单（ActionSheet）进行选择，大幅节省屏幕空间

--- iClassicTV/iClassicTV/Features/Settings/PlayerSettingsViewController.m
+++ iClassicTV/iClassicTV/Features/Settings/PlayerSettingsViewController.m
@@ -10,6 +10,10 @@
 #import "PlayerConfigManager.h"
 #import "LanguageManager.h"
 
+// [修改] 增加 UIActionSheetDelegate 协议，用于处理弹出选项
+@interface PlayerSettingsViewController () <UIActionSheetDelegate>
+@end
+
 @implementation PlayerSettingsViewController
 
 - (instancetype)init {
@@ -32,13 +36,9 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (section == 0) {
-        return 2; // 播放器类型: 自定义、原生
-    } else if (section == 1) {
-        return 3; // 全屏逻辑: 跟随系统、默认横屏、默认竖屏
-    } else if (section == 2) {
-        return 2; // 自定义播放器高级设置: 显示节目单、显示时间
-    }
+    if (section == 0) return 1; // [修改] 播放器选择改为1行
+    if (section == 1) return 1; // [修改] 全屏逻辑改为1行
+    if (section == 2) return 2; // 自定义播放器高级设置: 显示节目单、显示时间
     return 0;
 }
 
@@ -57,41 +57,38 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"PlayerSettingCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-    }
-    
-    // 重置复用状态
-    cell.accessoryType = UITableViewCellAccessoryNone;
-    cell.accessoryView = nil;
-    cell.textLabel.textColor = [UIColor blackColor];
-    
-    if (indexPath.section == 0) {
-        if (indexPath.row == 0) {
-            cell.textLabel.text = LocalizedString(@"custom_player_recommended");
-            if ([PlayerConfigManager preferredPlayerType] == 0) {
-                cell.accessoryType = UITableViewCellAccessoryCheckmark;
-                cell.textLabel.textColor = [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0];
-            }
-        } else if (indexPath.row == 1) {
-            cell.textLabel.text = LocalizedString(@"ios_native_player");
-            if ([PlayerConfigManager preferredPlayerType] == 1) {
-                cell.accessoryType = UITableViewCellAccessoryCheckmark;
-                cell.textLabel.textColor = [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0];
-            }
+    // [修改] 区分不同类型的 Cell 样式
+    if (indexPath.section == 0 || indexPath.section == 1) {
+        static NSString *Value1CellId = @"Value1Cell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:Value1CellId];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:Value1CellId];
         }
-    } else if (indexPath.section == 1) {
-        NSArray *titles = @[LocalizedString(@"follow_system"), LocalizedString(@"landscape"), LocalizedString(@"portrait")];
-        cell.textLabel.text = titles[indexPath.row];
         
-        if ([PlayerConfigManager preferredInterfaceOrientationPref] == indexPath.row) {
-            cell.accessoryType = UITableViewCellAccessoryCheckmark;
-            cell.textLabel.textColor = [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.textLabel.textColor = [UIColor blackColor];
+        
+        if (indexPath.section == 0) {
+            cell.textLabel.text = @"播放器选择";
+            cell.detailTextLabel.text = ([PlayerConfigManager preferredPlayerType] == 0) ? LocalizedString(@"custom_player_recommended") : LocalizedString(@"ios_native_player");
+        } else if (indexPath.section == 1) {
+            cell.textLabel.text = LocalizedString(@"default_fullscreen_logic");
+            NSArray *titles = @[LocalizedString(@"follow_system"), LocalizedString(@"landscape"), LocalizedString(@"portrait")];
+            cell.detailTextLabel.text = titles[[PlayerConfigManager preferredInterfaceOrientationPref]];
         }
-    } else if (indexPath.section == 2) {
+        
+        return cell;
+    } else {
+        static NSString *SwitchCellId = @"SwitchCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SwitchCellId];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:SwitchCellId];
+        }
+        
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.textLabel.textColor = [UIColor blackColor];
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        
         UISwitch *switchView = [[UISwitch alloc] initWithFrame:CGRectZero];
         if (indexPath.row == 0) {
             cell.textLabel.text = @"全屏状态显示节目单";
@@ -103,9 +100,9 @@
             [switchView addTarget:self action:@selector(timeSwitchChanged:) forControlEvents:UIControlEventValueChanged];
         }
         cell.accessoryView = switchView;
+        
+        return cell;
     }
-    
-    return cell;
 }
 
 #pragma mark - Table view delegate
@@ -113,13 +110,40 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
+    // [修改] 点击后弹出 ActionSheet 进行选择
     if (indexPath.section == 0) {
-        [PlayerConfigManager setPreferredPlayerType:indexPath.row];
-        // 播放器类型改变时，需要刷新整个列表以展示或隐藏 Section 2
-        [tableView reloadData];
+        UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:@"播放器选择"
+                                                           delegate:self
+                                                  cancelButtonTitle:LocalizedString(@"cancel")
+                                             destructiveButtonTitle:nil
+                                                  otherButtonTitles:LocalizedString(@"custom_player_recommended"), LocalizedString(@"ios_native_player"), nil];
+        sheet.tag = 100;
+        [sheet showInView:self.view];
     } else if (indexPath.section == 1) {
-        [PlayerConfigManager setPreferredInterfaceOrientationPref:indexPath.row];
-        [tableView reloadSections:[NSIndexSet indexSetWithIndex:1] withRowAnimation:UITableViewRowAnimationNone];
+        UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:LocalizedString(@"default_fullscreen_logic")
+                                                           delegate:self
+                                                  cancelButtonTitle:LocalizedString(@"cancel")
+                                             destructiveButtonTitle:nil
+                                                  otherButtonTitles:LocalizedString(@"follow_system"), LocalizedString(@"landscape"), LocalizedString(@"portrait"), nil];
+        sheet.tag = 101;
+        [sheet showInView:self.view];
+    }
+}
+
+#pragma mark - UIActionSheetDelegate
+
+// [新增] 处理选项结果并刷新表格
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == actionSheet.cancelButtonIndex) {
+        return;
+    }
+    
+    if (actionSheet.tag == 100) {
+        [PlayerConfigManager setPreferredPlayerType:buttonIndex];
+        [self.tableView reloadData];
+    } else if (actionSheet.tag == 101) {
+        [PlayerConfigManager setPreferredInterfaceOrientationPref:buttonIndex];
+        [self.tableView reloadData];
     }
 }
 
